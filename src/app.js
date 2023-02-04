55b90025d8dde0229c11e5f6a621deba121aea5d
Awais Yafai <awaisyafai0079@gmail.com>
2023-02-04 15:56:43 +0530

    props

--- src/app.js
+++ src/app.js
@@ -1,17 +1,55 @@
-import ExpenceItem from "./components/ExpenceItems";
+import ExpenceItems from "./components/ExpenceItems";
 
 function App() {
-  let expenseDate = new Date(2023, 1, 2);
-  let expenseTitle = "School Fee";
-  let expenseAmount = 300;
+  let expence = [
+    {
+      id: "e1",
+      title: "school fee",
+      amount: 300,
+      date: new Date(2023, 5, 2),
+    },
+    {
+      id: "e2",
+      title: "college fee",
+      amount: 500,
+      date: new Date(2023, 5, 2),
+    },
+    {
+      id: "e3",
+      title: "tution fee",
+      amount: 400,
+      date: new Date(2023, 5, 2),
+    },
+    {
+      id: "e4",
+      title: "bus fee",
+      amount: 200,
+      date: new Date(2023, 5, 2),
+    },
+  ];
   return (
     <div>
       <h2>Lets Get Started</h2>
-      <ExpenceItem
-        date={expenseDate}
-        title={expenseTitle}
-        amount={expenseAmount}
-      ></ExpenceItem>
+      <ExpenceItems
+        date={expence[0].date}
+        title={expence[0].title}
+        amount={expence[0].amount}
+      ></ExpenceItems>
+      <ExpenceItems
+                date={expence[1].date}
+                title={expence[1].title}
+                amount={expence[1].amount}
+      ></ExpenceItems>
+      <ExpenceItems
+                date={expence[2].date}
+                title={expence[2].title}
+                amount={expence[2].amount}
+      ></ExpenceItems>
+      <ExpenceItems
+               date={expence[3].date}
+               title={expence[3].title}
+               amount={expence[3].amount}
+      ></ExpenceItems>
     </div>
   );
 }
--- src/components/ExpenceItems.js
+++ src/components/ExpenceItems.js
@@ -1,19 +1,25 @@
+import React from "react";
 import "./ExpenceItem.css";
 
-function ExpenceItem(){
-    let title = 'Car Insurence'
-return(
-    <div className="expense-item">
-        <div className="date">Feb 1st 2023</div>
-        
-        <div className="expense-item__description">
-            <h2>{title}</h2>
-        </div>
-        <div className="expense-item__price">$200</div>
+function ExpenceItems(props) {
+    const month = props.date.toLocaleString('en-US',{month: 'long'})
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString('en-US',{day: '2-digit'})
+  // console.log(props.date);
+  return (
+    <div className="expence-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+
+      <div className="expence-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expence-item__price">${props.amount}</div>
     </div>
-)
-
-
+  );
 }
 
-export default ExpenceItem;
+export default ExpenceItems;

4e24625f04830d6619557c237513a94ecb397bc2
Awais Yafai <awaisyafai0079@gmail.com>
2023-02-04 14:27:09 +0530

    props

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import ExpenceItem from "./components/ExpenceItems";
 
-function App (){
-    return (
-        <div>
-            <h2>Lets Get Started</h2>
-    <ExpenceItem></ExpenceItem>
+function App() {
+  let expenseDate = new Date(2023, 1, 2);
+  let expenseTitle = "School Fee";
+  let expenseAmount = 300;
+  return (
+    <div>
+      <h2>Lets Get Started</h2>
+      <ExpenceItem
+        date={expenseDate}
+        title={expenseTitle}
+        amount={expenseAmount}
+      ></ExpenceItem>
     </div>
-    )
+  );
 }
 
 export default App;
--- src/components/ExpenceItems.js
+++ src/components/ExpenceItems.js
@@ -1,12 +1,13 @@
 import "./ExpenceItem.css";
 
 function ExpenceItem(){
+    let title = 'Car Insurence'
 return(
     <div className="expense-item">
         <div className="date">Feb 1st 2023</div>
         
         <div className="expense-item__description">
-            <h2>Car Insurense</h2>
+            <h2>{title}</h2>
         </div>
         <div className="expense-item__price">$200</div>
     </div>

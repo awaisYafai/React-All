af77e61874cb0e8ee7cbc955ec4d37a8badf67f4
Awais Yafai <awaisyafai0079@gmail.com>
2023-02-07 19:29:11 +0530

    state

--- src/components/Expences/ExpenceItems.js
+++ src/components/Expences/ExpenceItems.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenceItem.css";
 import ExpenceDate from "./ExpenceDate";
 import Card from "../UI/Card";
 const ExpenceItems =(props)=> {
   // console.log(props.date);
+
+  const clickHandler = ()=>{
+  }
   return (
     <Card className="expence-item">
       <ExpenceDate date={props.date} />
       <div className="expence-item__description">
-        <h2>{props.title}</h2>
+        <h2>{}</h2>
       </div>
       <div className="expence-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }

60279ff8662572cf6ae4c356d6d8a46113e5148b
Awais Yafai <awaisyafai0079@gmail.com>
2023-02-09 18:05:39 +0530

    form

--- src/components/NewExpences/ExpenceForm.js
+++ src/components/NewExpences/ExpenceForm.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenceForm.css";
 
 const ExpenceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHAndler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHAndler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHAndler = (event) => {
+    setEnteredDate(event.target.value);
+  };
   return (
     <form>
       <div className="new-expence__controls">
         <div className="new-expence__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHAndler} />
         </div>
       </div>
       <div className="new-expence__controls">
         <div className="new-expence__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHAndler}
+          />
         </div>
       </div>
       <div className="new-expence__controls">
         <div className="new-expence__control">
           <label>Date</label>
-          <input type="date" />
+          <input type="date" onChange={dateChangeHAndler} />
         </div>
       </div>
       <div className="new-expence__action">
